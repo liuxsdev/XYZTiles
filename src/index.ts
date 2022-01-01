1fed9dbdfd5e0286ba598eb64edfd87bc3b3bf55
smilevent <1028330144@qq.com>
2022-01-01 11:09:41 +0800

    add google maps

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import { TileLayer, TileLayerOptions } from "leaflet";
 
+/**
+ * 天地图XYZ Tiles
+ * 官方网站地图API说明:http://lbs.tianditu.gov.cn/server/MapService.html
+ * 申请key(accesstoken):https://console.tianditu.gov.cn/api/key
+ * subdomains: "01234567",
+ * 图层说明:
+ * vec_w 矢量底图 Vector 球面墨卡托投影(WGS84)
+ * cva_w 矢量注记 Chinese Vector Annotation 球面墨卡托投影(WGS84)
+ */
 class Tianditu {
-    /**
-     * 天地图XYZ Tiles
-     * 官方网站地图API说明：http://lbs.tianditu.gov.cn/server/MapService.html
-     * subdomains: "01234567",
-     * vec_w 矢量底图 球面墨卡托投影
-     * cva_w 矢量注记 球面墨卡托投影
-     */
     accessToken: string;
     options: TileLayerOptions = {
         subdomains: "01234567",
         maxZoom: 20,
-        attribution: 'Map data &copy; <a href="https://www.tianditu.gov.cn">天地图</a>',
+        attribution: 'Map Data &copy; <a href="https://www.tianditu.gov.cn">天地图</a>',
         accessToken: "",
     };
     tileUrls = {
@@ -33,8 +35,37 @@ class Tianditu {
     }
 }
 
+/**
+ * 谷歌地图
+ * url:http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}
+ * subdomains:['mt0','mt1','mt2','mt3']
+ * maxzoom:20
+ * h = roads only
+ * m = standard roadmap
+ * p = terrain
+ * r = somehow altered roadmap
+ * s = satellite only 影像图
+ * t = terrain only 地形图
+ * y = hybrid
+ *
+ */
+class GoogleMap {
+    options: TileLayerOptions = {
+        subdomains: ["mt0", "mt1", "mt2", "mt3"],
+        maxZoom: 20,
+        attribution: 'Map Data &copy; <a href="https://developers.google.com/maps">Google Maps</a>',
+    };
+    private getUrl(lyrs: string) {
+        return `http://{s}.google.com/vt/lyrs=${lyrs}&x={x}&y={y}&z={z}`;
+    }
+    satellite() {
+        return new TileLayer(this.getUrl("s"), this.options);
+    }
+}
+
 const XYZTiles = {
     tianditu: Tianditu,
+    googlemap: GoogleMap,
 };
 
 export { XYZTiles };
